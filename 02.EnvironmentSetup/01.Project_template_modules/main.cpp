92a4034e55f8a35f5f0987463a0f1f5cb1c48662
GregWagner <theGreg101@gmail.com>
2025-09-09 15:54:16 -0500

    Checking if all is well

--- 02.EnvironmentSetup/01.Project_template_modules/main.cpp
+++ 02.EnvironmentSetup/01.Project_template_modules/main.cpp
@@ -2,7 +2,7 @@
 import utilities;
 
 int main() {
-    int value1 {15} ;
+    int value1 {10} ;
     int value2 {20};
     int result {value1 + value2};
     fmt::print("The result is: {}\n", result);
